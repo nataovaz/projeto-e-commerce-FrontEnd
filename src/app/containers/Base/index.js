035ec43f4e3a9029c12b3c1a8f437e5b8f73c257
Natan Vaz <natanvaz27@gmail.com>
2022-12-11 00:08:26 -0300

    Estilização do modelo

--- src/app/containers/Base/index.js
+++ src/app/containers/Base/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 
+const BarraTopo = () => (
+    <div className='flex horizontal full-width'>
+        <div className='flex-1 flex flex-start'>
+            <p>Ver Loja</p>
+        </div>
+        <div className='flex-1 flex flex-end'>
+            <p>Sair</p>
+        </div>
+    </div>
+)
 class Base extends React.Component{
     render(){
         return(
-            <div className='flex horizontal'>
+            <div className='flex horizontal full-height'>
                 <div className='flex vertical'>
                     <p>Menu</p>
                 </div>
-                <div className='flex vertical'>
-                    <div className='flex horizontal'>
-                        Barra do Topo
+                <div className='flex vertical full-width'>
+                    <div className='flex horizontal '>
+                        <BarraTopo />
                     </div>
                     <div className='flex'>
                         {this.props.children}
